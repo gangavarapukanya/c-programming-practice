fa940bd77bf2a97f9bf145f98ccc512c2097dd5d
gangavarapukanya <gangavarapukanya@gmail.com>
2026-09-14 18:51:01 +0530

    Create operators.c for basic input of two numbers
    Initial implementation of a C program to enter two numbers.

---
+++ operators.c
@@ -0,0 +1,11 @@
+/*Aim:write a c program to enter the operators
+Date:14/09/26
+username :kanya G.
+ #include<stdio.h>
+ int main()
+  {
+     int a=2,b=3;
+ printf("enter the value of two numbers\n ");
+ scanf ("%d%d,&a,&b\n");
+return 0;
+}
