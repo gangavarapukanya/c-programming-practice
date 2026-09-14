4f2a3c8276e980a5d9dec35cdc3a27aae11e91ba
gangavarapukanya <gangavarapukanya@gmail.com>
2026-09-14 20:59:51 +0530

    Add C program to demonstrate logical operators
    This program demonstrates the use of logical operators in C by evaluating conditions and printing the results .

---
+++ logical operators.c
@@ -0,0 +1,13 @@
+/*Aim:write a c program for to print the logical operators
+Date:14/09/26
+username:kanya*/
+#include<stdio.h>
+int main()
+{
+   int i=1,j=2,k=3;
+  printf("enter the value of two numbers ");
+  printf("(i>j&&i>k):%d\n ",(i>j&&i>k));
+  printf("(i<j&&j<k):&d\n",(i<j&&i<k));
+ printf("(i!=2):%d\n",(i!=2));
+return 0;
+}
