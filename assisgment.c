05d97b27b5a9a01a5cc9d6f2f261cc2647994e3e
gangavarapukanya <gangavarapukanya@gmail.com>
2026-09-15 20:25:44 +0530

    Add files via upload

---
+++ assisgment.c
@@ -0,0 +1,17 @@
+/*Aim: write a c program for to print assisgment operators.c
+Date: 15/09/26
+user name : kanya G. */
+#include<stdio.h>
+int main()
+{
+	 int a=10;
+	 a=10;//a=a+10;
+	 printf("%d\n",a);
+	 a-=2;//a=a-2;
+	 printf("%d\n",a);
+	 a*=2;//a=*2;
+	 printf("%d\n",a);
+	 a/=2;//a/2;
+	 printf("%d\n",a);
+	 return 0;
+}
